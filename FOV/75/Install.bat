3c002f565d34a9a2bd165fcd999a029c3464512f
PkmnYellow <108107477+PkmnYellow@users.noreply.github.com>
2022-08-28 17:23:06 -0500

    Add files via upload

---
+++ FOV/75/Install.bat
@@ -0,0 +1,42 @@
+@echo off
+cd /D "%~dp0"
+set var=%1
+
+REM --Checks if the directory variable exists. Otherwise, the batch file will terminate.--
+if defined var ( echo Universal variable defined...
+goto :install_u
+) else ( echo Universal variable undefined... 
+goto :end )
+
+REM --If checks are established to create the necessary backup files for installation.--
+:install_u
+if exist %var%\Backup\KellerGame\CookedPC\Packages\Camera\CameraConfig.uppc" ( echo CameraConfig found...
+) else ( echo Adding CameraConfig.uppc to Backup...
+mkdir %var%\Backup\KellerGame\CookedPC\Packages\Camera" 2>nul 
+copy /Y %var%\KellerGame\CookedPC\Packages\Camera\CameraConfig.uppc" %var%\Backup\KellerGame\CookedPC\Packages\Camera" )
+
+:installmod_u
+echo Installing FOV...
+robocopy KellerGame %var%\KellerGame" /e
+echo Installation complete!
+goto :modcheck
+
+REM --Checks if installedmods.txt exists or not.--
+:modcheck
+cd ..\..
+if exist installedmods.txt ( goto :modcheck2
+) else ( echo FOV 75 >> installedmods.txt 
+echo Installedmods.txt created...
+goto :end )
+
+REM --Checks if "FOV 75" is logged into installedmods.txt.--
+:modcheck2
+set results9=
+for /f "tokens=1 delims=" %%e in ('findstr /i "FOV 75" installedmods.txt') do set results9=%%e
+
+if defined results9 ( goto :end
+) else ( echo FOV 75 >> installedmods.txt 
+echo FOV 75 added to the mod list!
+goto :end )
+
+:end
